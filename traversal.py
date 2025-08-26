24b6728b8d9de4e7245e93df72187c2fd283b8e0
jml-moe <2318069@student.itn.ac.id>
2025-08-26 19:34:09 +0700

    Add traversal examples for type conversion, list manipulation, and dictionary usage; create .gitignore for environment files

--- traversal.py
+++ traversal.py
@@ -25,3 +25,30 @@ print(my_prompt)  # Mencetak string mentah dengan placeholder {fullname} tanpa p
 print(my_prompt.format(fullname=fullname))  # Mencetak string setelah mengganti {fullname} dengan nilai fullname yang sebenarnya
 
 
+#traversal bisa ubah float ke int pun sebaliknya
+my_age = 20
+my_ipk = 3.79
+
+print(float(my_age))
+print(int(my_ipk))
+
+#list 
+my_fruits = ["apple", "durian", "mango"]
+my_fruits.append("lemon")
+
+print(my_fruits)
+
+#tuple -> bisa di unpack (destructuring)
+a, b = 0,1
+print(a,b)
+
+#dictionary -> bisa di get
+user = {
+    "name" : "Jamil",
+    "age" : 20,
+    "hobby" : "reading"
+}
+
+user["is_student"] = True
+print(user.get("height", "-")) #set default value
+print(user)
