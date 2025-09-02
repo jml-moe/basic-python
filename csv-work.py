cf4949fe77d7b72fff084a2fdd393a0dc1c647a9
jml-moe <2318069@student.itn.ac.id>
2025-09-02 14:30:49 +0700

    Refactor CSV reading in csv-work.py to improve clarity and integrate Streamlit for data visualization

--- csv-work.py
+++ csv-work.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import streamlit as st
 
+"""
 with open("data.csv", "r") as file:
     # print(file.read()) -> tanpa pandas
     csv = pd.read_csv(file)
     print(csv)
+"""
+
+with open("data.csv", "r") as file:
+    csv = pd.read_csv(file)
+
+    st.write("### Data Employe")
+    st.dataframe(csv)
+
+    st.bar_chart(csv.set_index("city")["salary"])
